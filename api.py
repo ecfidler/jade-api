f68f6b21ecf9cfa157c063c78eaa0a8d61e14487
Ethan Fidler <ethancf1999@gmail.com>
2023-03-13 02:29:46 -0400

    adding filemanager

--- api.py
+++ api.py
@@ -1,33 +1,58 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException, status
+# from fastapi.responses import JSONResponse
+
 from random import randint
 
-app = FastAPI(title="JADE api")
+from .filemanager import get_file_listing, read_file_from_filename, save_file
+
+tags = [
+    {
+        "name": "Files",
+        "description": "Files stored in storage on the printer"
+    },
+    {
+        "name": "Status",
+        "description": "Physical data collected by the printer"
+    }
+]
+
+app = FastAPI(title="JADE API",
+              description="The API that the JADE interface uses to interact with the printer",
+              openapi_tags=tags)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
 
+
 @app.get("/ping")
 async def test():
     return {"message": "pong!"}
 
-@app.get("/files")
-async def filesListing():
-    tempfiles = [
-        { "name": "fileone.stl" },
-        { "name": "filetwo.stl" },
-        { "name": "filethree.stl" },
-        { "name": "3dBenchy.stl" },
-    ]
 
-    return tempfiles
+@app.get("/files", tags=["Files"])
+async def files_listing():
+    return get_file_listing()
 
-# @app.get("/file/{name}")
-# async def getFile():
-#     # load and send stl file
-#     return
 
-@app.get("/printerStatus")
-async def printerStatus():
+# https://www.tutorialspoint.com/fastapi/fastapi_uploading_files.htm
+@app.put("/file/{name}", tags=["Files"])
+async def put_file(name: str, file: UploadFile):
+    save_file(name, file.read())
+    return {"file_size": len(file)}
+
+
+@app.get("/file/{name}", tags=["Files"])
+async def get_file(name: str):
+    try:
+        return {"name": name, "data": read_file_from_filename(name)}
+    except FileNotFoundError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"File not found: {name}")
+
+
+@app.get("/printerStatus", tags=["Status"])
+async def get_printer_status():
     # send randomized value for temperatures
-    return {"status": "randomzied Data", "bedTemperature": randint(0,100), "hotEndTemperature": randint(0, 400)}
+    return {"status": "randomzied Data", "bedTemperature": randint(0, 100), "hotEndTemperature": randint(0, 400)}
---
+++ filemanager.py
@@ -0,0 +1,32 @@
+import glob
+import os
+from pathlib import Path
+
+PATH = Path('.') / 'files'
+FILETYPES = {'.stl', '.gcode'}
+
+
+def get_files_of_types(path: Path, types: set):
+    return list(p.resolve() for p in path.glob("**/*") if p.suffix in types)
+
+
+def get_file_listing():
+    files = get_files_of_types(PATH, FILETYPES)
+    return [{"name": file.name, "type": file.suffix} for file in files]
+
+
+def read_file_from_filename(filename: str):
+    filepath = PATH / filename
+    if not filepath.is_file():
+        raise FileNotFoundError()
+    with open(filepath, 'rb') as f:
+        data = f.read()
+    return data
+
+
+def save_file(name: str, file: bytes):
+    if not file:
+        raise ValueError
+    filepath = PATH / name
+    size = filepath.write_bytes(file)
+    return {'file': filepath, 'bytes': size}
